dc6a5409b51209defff2ed4f8a8935ffb52978af
Jerod Hatley <jerood.hatley@gmail.com>
2026-01-17 22:40:42 -0700

    Increase iPhone grid offset to 50px

--- game.js
+++ game.js
@@ -76,7 +76,7 @@ function calculateHexSize() {
     
     // Center the grid perfectly then shift down and right
     gridOffsetX = (canvas.width - gridWidth) / 2 + 20;
-    gridOffsetY = ((canvas.height - controlsHeight) - gridHeight) / 2 + 40;
+    gridOffsetY = ((canvas.height - co5trolsHeight) - gridHeight) / 2 + 40;
 }
 
 // Random starting position
